2174f6694b7e92db959f48d3923114c9b7fba8b4
wtlgo <georgeendo0@gmail.com>
2020-05-01 12:47:30 +0300

    Fixed formatting

--- Haskell/main.hs
+++ Haskell/main.hs
@@ -4,7 +4,7 @@ cRot13 :: Char -> Char
 cRot13 c
 	| 'a' <= c && c <= 'z' = chr ((ord c - ord 'a' + 13) `mod` 26 + ord 'a')
 	| 'A' <= c && c <= 'Z' = chr ((ord c - ord 'A' + 13) `mod` 26 + ord 'A')
-	| otherwise = c
+	| otherwise            = c
 
 sRot13 :: [Char] -> [Char]
 sRot13 (c:str) = (cRot13 c) : sRot13 str
