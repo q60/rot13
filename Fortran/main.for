e49119ba2ac6d38f715e4a5fcf4bb9b85d950333
llathasa-veleth <llathasa@outlook.com>
2020-05-04 17:20:59 +0300

    Befunge added

--- Fortran/main.for
+++ Fortran/main.for
@@ -1,24 +1,24 @@
 program ROT13
-    character (len = 256) :: str
-    integer :: c 
+  character (len = 256) :: str
+  integer :: c 
+  
+  write (*, '(A)') "Enter string to encode: "
+  read (*, '(A)') str
+  
+  do i = 1, 256
+    c = ichar(str(i:i))
     
-    write (*, '(A)') "Enter string to encode: "
-    read (*, '(A)') str
+    if (ichar('A') <= c .and. c <= ichar('Z')) then
+      c = mod(c - ichar('A') + 13, 26) + ichar('A')
+    else if (ichar('a') <= c .and. c <= ichar('z')) then
+      c = mod(c - ichar('a') + 13, 26) + ichar('a')
+    end if
     
-    do i = 1, 256
-        c = ichar(str(i:i))
-        
-        if (ichar('A') <= c .and. c <= ichar('Z')) then
-            c = mod(c - ichar('A') + 13, 26) + ichar('A')
-        else if (ichar('a') <= c .and. c <= ichar('z')) then
-            c = mod(c - ichar('a') + 13, 26) + ichar('a')
-        end if
-        
-        str(i:i) = char(c)
-        cycle
-    enddo
-    
-    write (*, '(A,A)') "Encoded string: ", str
-    
-    stop
+    str(i:i) = char(c)
+    cycle
+  enddo
+  
+  write (*, '(A,A)') "Encoded string: ", str
+  
+  stop
 end
