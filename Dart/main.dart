eac847fe1b8b5239149a0853bcefb20020e2e424
llathasa-veleth <llathasa@outlook.com>
2020-05-05 23:25:28 +0300

    Dart added

---
+++ Dart/main.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+void main() {
+  stdout.writeln("Enter string to encode:");
+  String toEncode = stdin.readLineSync();
+  String encoded  = "";
+  toEncode.runes.forEach(
+    (int rune) {
+      int charNormal = String.fromCharCode(rune).codeUnitAt(0);
+      int charUpper  = String.fromCharCode(rune).toUpperCase().codeUnitAt(0);
+      if (charUpper <= 77 && 65 <= charUpper) {
+        encoded += String.fromCharCode(charNormal + 13);
+      } else if (charUpper <= 90 && 77 <= charUpper) {
+        encoded += String.fromCharCode(charNormal - 13);
+      } else {
+        encoded += String.fromCharCode(charNormal);
+      }
+    }
+  );
+  stdout.writeln("Encoded string:\n$encoded");
+}
