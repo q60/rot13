e626609dd3cf2c56bc9096ada628caadab71c789
wtlgo <georgeendo0@gmail.com>
2020-05-05 13:49:37 +0300

    Made indentations adequate

--- C#/main.cs
+++ C#/main.cs
@@ -3,17 +3,17 @@ using System.Linq;
 
 public class Rot13
 {
-	public static void Main()
-	{
-		Console.Write("Enter string to encode: ");
-		Console.Write(String.Format("Encoded string: {0}\n",
-			new String(
-				Console.ReadLine().ToCharArray().Select(c => {
-					if('a' <= c && c <= 'z') c = (char)((c - 'a' + 13) % 26 + 'a');
-					else if('A' <= c && c <= 'Z') c = (char)((c - 'A' + 13) % 26 + 'A');
-					return c;
-				}).ToArray()
-			)
-		));
-	}
+    public static void Main()
+    {
+        Console.Write("Enter string to encode: ");
+        Console.Write(String.Format("Encoded string: {0}\n",
+            new String(
+                Console.ReadLine().ToCharArray().Select(c => {
+                    if('a' <= c && c <= 'z') c = (char)((c - 'a' + 13) % 26 + 'a');
+                    else if('A' <= c && c <= 'Z') c = (char)((c - 'A' + 13) % 26 + 'A');
+                    return c;
+                }).ToArray()
+            )
+        ));
+    }
 }
