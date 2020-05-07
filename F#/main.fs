cee3b4f09e3402eef5e9928de4d8148902c9b5ef
llathasa-veleth <llathasa@outlook.com>
2020-05-08 02:00:20 +0300

    F# added

---
+++ F#/main.fs
@@ -0,0 +1,24 @@
+open System
+
+let rotateString toEncode : string =
+  toEncode
+  |> Array.ofSeq
+  |> Array.map (
+    int >> (
+      fun ch ->
+        match ch with
+        | ch when (ch >= 65 && ch <= 77) || (ch >= 97 && ch <= 109) -> ch + 13
+        | ch when (ch >= 78 && ch <= 90) || (ch >= 110 && ch <= 122) -> ch - 13
+        | _ -> ch
+      ) >> char
+  )
+  |> System.String
+  |> string
+
+[<EntryPoint>]
+let main argv : int =
+  printfn "Enter string to encode:"
+  let toEncode = Console.ReadLine ()
+  printfn "Encoded string:"
+  printfn "%s" (rotateString toEncode)
+  0
