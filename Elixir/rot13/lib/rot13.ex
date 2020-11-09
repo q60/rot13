31a242dac159093250d5cac7e4357bd6ea033e49
Llathasa Veleth <llathasa@outlook.com>
2020-11-09 17:30:57 +0300

    Elixir updated

--- Elixir/rot13/lib/rot13.ex
+++ Elixir/rot13/lib/rot13.ex
@@ -1,33 +1,38 @@
 defmodule Rot13 do
   @spec start(any, any) :: {:ok, pid}
-  @spec rotate(any) :: :ok
+  @spec rotate(any) :: any
 
   def rotate(xs) do
-    IO.puts("Encoded string:")
-    for x <- xs do
-      y = << x :: utf8 >>
-        |> String.upcase()
-        |> String.to_charlist()
-        |> Enum.at(0)
-      if 65 <= y and y <= 90 do
-        c = <<
-              x + (-13 * trunc(abs(y - 77.5)/(y - 77.5))) :: utf8
-            >>
-        IO.write(c)
-      else
-        c = << x :: utf8 >>
-        IO.write(c)
-      end
-    end
-    IO.write("\n")
+    xs
+      |> Enum.map(
+        fn c ->
+          x = << c :: utf8 >>
+            |> String.upcase()
+            |> String.to_charlist()
+            |> Enum.at(0)
+          if 65 <= x and x <= 90 do
+            c = <<
+                  c + (-13 * trunc(abs(x - 77.5)/(x - 77.5))) :: utf8
+                >>
+            c
+          else
+            c = << c :: utf8 >>
+            c
+          end
+        end)
   end
 
   def start(_type, _args) do
     IO.puts("Enter string to encode:")
-    IO.read(:stdio, :line)
-      |> String.to_charlist()
-      |> List.delete_at(-1)
-      |> rotate()
+    IO.puts(
+      [
+        "Encoded string:\n",
+        IO.read(:stdio, :line)
+          |> String.to_charlist()
+          |> List.delete_at(-1)
+          |> rotate()
+      ]
+    )
     Task.start(fn -> :timer.sleep(1000) end)
   end
 end
