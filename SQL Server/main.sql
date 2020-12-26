5e954fd1d0c3b8f3bfb062069bffea9d2761f5d8
Llathasa Veleth <llathasa@outlook.com>
2020-12-26 13:47:47 +0300

    SQL Server: updated indentation

--- SQL Server/main.sql
+++ SQL Server/main.sql
@@ -7,19 +7,19 @@ declare @i int = 0;
 declare @c char;
 while @i < len(@input)
 begin
-    set @i = @i + 1;
-    set @c = substring(@input, @i, 1);
+  set @i = @i + 1;
+  set @c = substring(@input, @i, 1);
 
-    if ascii('a') <= ascii(@c) and ascii(@c) <= ascii('z')
-    begin
-        set @c = char((ascii(@c) - ascii('a') + 13) % 26 + ascii('a'));
-    end
-    else if ascii('A') <= ascii(@c) and ascii(@c) <= ascii('Z')
-    begin
-        set @c = char((ascii(@c) - ascii('A') + 13) % 26 + ascii('A'));
-    end
+  if ascii('a') <= ascii(@c) and ascii(@c) <= ascii('z')
+  begin
+    set @c = char((ascii(@c) - ascii('a') + 13) % 26 + ascii('a'));
+  end
+  else if ascii('A') <= ascii(@c) and ascii(@c) <= ascii('Z')
+  begin
+    set @c = char((ascii(@c) - ascii('A') + 13) % 26 + ascii('A'));
+  end
 
-    set @output = @output + @c;
+  set @output = @output + @c;
 end
 
 select @output as result;
