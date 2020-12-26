02b08656602ddea2ea6370768f68ef568a70d686
Llathasa Veleth <llathasa@outlook.com>
2020-12-26 13:25:54 +0300

    C++: updated indentation

--- C++/main.cpp
+++ C++/main.cpp
@@ -2,22 +2,22 @@
 #include <iterator>
 
 int main() {
-    std::cout << "Enter string to encode: ";
+  std::cout << "Enter string to encode: ";
 
-    const std::string inp {
-        []{
-            std::string inp;
-            std::getline(std::cin, inp);
+  const std::string inp {
+    []{
+      std::string inp;
+      std::getline(std::cin, inp);
 
-            return inp;
-        }()
-    };
+      return inp;
+    }()
+  };
 
-    std::cout << "Encoded string: ";
-    std::transform(inp.cbegin(), inp.cend(), std::ostream_iterator<char> { std::cout }, [](char c) {
-        return isalpha(c) ? ((c - (islower(c) ? 'a' : 'A') + 13) % 26 + (islower(c) ? 'a' : 'A')) : c;
-    });
-    std::cout << std::endl;
+  std::cout << "Encoded string: ";
+  std::transform(inp.cbegin(), inp.cend(), std::ostream_iterator<char> { std::cout }, [](char c) {
+    return isalpha(c) ? ((c - (islower(c) ? 'a' : 'A') + 13) % 26 + (islower(c) ? 'a' : 'A')) : c;
+  });
+  std::cout << std::endl;
 
-    return 0;
+  return 0;
 }
