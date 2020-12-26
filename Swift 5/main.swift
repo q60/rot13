48222c8b91ae81c3d20805ba93174b276733e396
Llathasa Veleth <llathasa@outlook.com>
2020-12-26 13:48:25 +0300

    Swift 5: updated indentation

--- Swift 5/main.swift
+++ Swift 5/main.swift
@@ -2,16 +2,16 @@
 
 print("Enter string to encode: ", terminator: "")
 if let inp = readLine() {
-    let out = String(inp.map { (c) -> Character in
-        if c.isASCII && c.isLetter {
-            if let ascii_c = c.asciiValue {
-                return Character(UnicodeScalar((ascii_c - (c.isLowercase ? 97 : 65) + 13) % 26 + (c.isLowercase ? 97 : 65)))
-            }
-        }
-        return c
-    })
-    print("Encoded string: \(out)")
+  let out = String(inp.map { (c) -> Character in
+    if c.isASCII && c.isLetter {
+      if let ascii_c = c.asciiValue {
+        return Character(UnicodeScalar((ascii_c - (c.isLowercase ? 97 : 65) + 13) % 26 + (c.isLowercase ? 97 : 65)))
+      }
+    }
+    return c
+  })
+  print("Encoded string: \(out)")
 }
 else {
-    print("Input error. Aborting.")
+  print("Input error. Aborting.")
 }
