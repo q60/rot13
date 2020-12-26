1c233a63504b9789c9bacbf991646a7492ca1689
Llathasa Veleth <llathasa@outlook.com>
2020-12-26 13:47:24 +0300

    SQL Server: removed trailing whitespaces

--- SQL Server/main.sql
+++ SQL Server/main.sql
@@ -9,7 +9,7 @@ while @i < len(@input)
 begin
     set @i = @i + 1;
     set @c = substring(@input, @i, 1);
-    
+
     if ascii('a') <= ascii(@c) and ascii(@c) <= ascii('z')
     begin
         set @c = char((ascii(@c) - ascii('a') + 13) % 26 + ascii('a'));
@@ -18,7 +18,7 @@ begin
     begin
         set @c = char((ascii(@c) - ascii('A') + 13) % 26 + ascii('A'));
     end
-    
+
     set @output = @output + @c;
 end
 
