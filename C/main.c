e68935e2bb1c25cbe236a69caa3558f07f8bc98b
Llathasa Veleth <llathasa@outlook.com>
2020-12-26 13:24:03 +0300

    C: updated indentation

--- C/main.c
+++ C/main.c
@@ -3,33 +3,33 @@
 #include <ctype.h>
 
 int main() {
-    printf("Enter string to encode: ");
+  printf("Enter string to encode: ");
 
-    size_t sbuff_len = 256;
-    char* sbuff = (char*)calloc(sbuff_len, sizeof(char));
+  size_t sbuff_len = 256;
+  char* sbuff = (char*)calloc(sbuff_len, sizeof(char));
 
-    char c;
-    size_t len = 0;
-    while((c = getchar()) != '\n') {
-        sbuff[len++] = isalpha(c) ? ((c - (islower(c) ? 'a' : 'A') + 13) % 26 + (islower(c) ? 'a' : 'A')) : c;
+  char c;
+  size_t len = 0;
+  while((c = getchar()) != '\n') {
+    sbuff[len++] = isalpha(c) ? ((c - (islower(c) ? 'a' : 'A') + 13) % 26 + (islower(c) ? 'a' : 'A')) : c;
 
-        if(len == sbuff_len) {
-            size_t new_len = sbuff_len + 256;
-            char* new_buff = (char*)realloc(sbuff, new_len * sizeof(char));
-            if(new_buff != NULL) {
-                sbuff_len = new_len;
-                sbuff = new_buff;
-            }
-            else {
-                printf("Input error. Aborting.\n");
-                return 1;
-            }
-        }
+    if(len == sbuff_len) {
+      size_t new_len = sbuff_len + 256;
+      char* new_buff = (char*)realloc(sbuff, new_len * sizeof(char));
+      if(new_buff != NULL) {
+        sbuff_len = new_len;
+        sbuff = new_buff;
+      }
+      else {
+        printf("Input error. Aborting.\n");
+        return 1;
+      }
     }
-    sbuff[len] = '\0';
+  }
+  sbuff[len] = '\0';
 
-    printf("Encoded string: %s\n", sbuff);
+  printf("Encoded string: %s\n", sbuff);
 
-    free(sbuff);
-    return 0;
+  free(sbuff);
+  return 0;
 }
