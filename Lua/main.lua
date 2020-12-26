0b058665747fb23f31c013294aac68bd93f832a9
Llathasa Veleth <llathasa@outlook.com>
2020-12-26 13:40:11 +0300

    Lua: updated indentation

--- Lua/main.lua
+++ Lua/main.lua
@@ -3,14 +3,14 @@ local s = io.read()
 
 local res = ''
 for i = 1, s:len() do
-    local c = s:byte(i)
-    if string.byte('a') <= c and c <= string.byte('z') then
-        c = (c - string.byte('a') + 13) % 26 + string.byte('a')
-    elseif string.byte('A') <= c and c <= string.byte('Z') then
-        c = (c - string.byte('A') + 13) % 26 + string.byte('A')
-    end
+  local c = s:byte(i)
+  if string.byte('a') <= c and c <= string.byte('z') then
+    c = (c - string.byte('a') + 13) % 26 + string.byte('a')
+  elseif string.byte('A') <= c and c <= string.byte('Z') then
+    c = (c - string.byte('A') + 13) % 26 + string.byte('A')
+  end
 
-    res = res .. string.char(c)
+  res = res .. string.char(c)
 end
 
 print('Encoded string: ' .. res)
