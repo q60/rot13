06f44c598a99c392847d71bc601a55385a01d67f
veleth <llathasa@outlook.com>
2020-12-28 18:43:14 +0300

    Added space, so there is a gap between prompt and text

--- Emacs Lisp/main.el
+++ Emacs Lisp/main.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (defun encode-rot13 (text) 
   "Cipher text with ROT13." 
-  (interactive "sEnter string to encode:") 
+  (interactive "sEnter string to encode: ") 
   (message (concat "Encoded string:\n" (rot13-string text))))
 
 ;;; main.el ends here
