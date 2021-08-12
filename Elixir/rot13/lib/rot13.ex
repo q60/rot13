691628f3d02d32b2ff91246849d78f696bcd05db
llathasa-veleth <llathasa@outlook.com>
2021-08-12 19:37:19 +0300

    Elixir: mix format

--- Elixir/rot13/lib/rot13.ex
+++ Elixir/rot13/lib/rot13.ex
@@ -3,28 +3,30 @@ defmodule Rot13 do
     cond do
       v in ?A..?M ->
         k + 13
+
       v in ?N..?Z ->
         k - 13
+
       true ->
         k
     end
   end
 
   def main(_) do
-    encoded = IO.gets("Enter string to encode:\n")
-    |> String.trim()
-    |> then(
-    fn xs ->
-      List.zip(
-        [
+    encoded =
+      IO.gets("Enter string to encode:\n")
+      |> String.trim()
+      |> then(fn xs ->
+        List.zip([
           xs
           |> String.to_charlist(),
           xs
           |> String.upcase()
           |> String.to_charlist()
         ])
-    end)
-    |> Enum.map(&rotate/1)
+      end)
+      |> Enum.map(&rotate/1)
+
     IO.puts("Encoded string:\n#{encoded}")
   end
 end
