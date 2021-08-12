a292baa394d27c37ce5d77cf3c961cdb09ec4075
llathasa-veleth <llathasa@outlook.com>
2021-08-12 19:38:21 +0300

    R: full re-write

--- R/main.R
+++ R/main.R
@@ -1,18 +1,24 @@
-print("Enter string to encode:")
+cat("Enter string to encode:\n")
+to_encode <- readLines("stdin", n = 1)
+
+zip <- matrix(
+    c(
+      gtools::asc(to_encode),
+      gtools::asc(toupper(to_encode))),
+    ncol = 2)
+
 encoded <- ""
 
-for (char in gtools::asc(to_encode)) {
-  if ((char >= 65) && (char <= 77)) {
-    encoded <- paste(encoded, gtools::chr(char+13), sep = "")
-  } else if ((char >= 97) && (char <= 109)) {
-    encoded <- paste(encoded, gtools::chr(char+13), sep = "")
-  } else if ((char >= 78) && (char <= 90)) {
-    encoded <- paste(encoded, gtools::chr(char-13), sep = "")
-  } else if ((char >= 110) && (char <= 122)) {
-    encoded <- paste(encoded, gtools::chr(char-13), sep = "")
-  } else {
-    encoded <- paste(encoded, gtools::chr(char), sep = "")
-  }
+for (row in 1:nrow(zip)) {
+    zip[row,1] -> k
+    zip[row,2] -> v
+    if (v >= 65 && v <= 77) {
+        encoded <- paste(encoded, gtools::chr(k+13), sep = "")
+    } else if (v >= 78 && v <= 90) {
+        encoded <- paste(encoded, gtools::chr(k-13), sep = "")
+    } else {
+        encoded <- paste(encoded, gtools::chr(k), sep = "")
+    }
 }
-print("Encoded string:")
-print(encoded)
+cat("Encoded string:\n")
+cat(encoded, "\n")
