e2d8c1b024dd615b3a69bfd9c2154be3f1b35643
llathasa-veleth <llathasa@outlook.com>
2022-07-27 20:12:43 +0300

    Update Racket

--- Racket/main.rkt
+++ Racket/main.rkt
@@ -1,34 +1,32 @@
 #lang racket
 
-(define (main)
-  (begin
-    (displayln "Enter string to encode:")
-    (define to-encode
-      (read-line
-        (current-input-port)))
+(begin
+  (displayln "Enter string to encode:")
+  (define to-encode
+    (read-line
+     (current-input-port)))
 
-    (define (rotate byte)
-      (begin
-        (define char-to-encode (char->integer byte))
-        (cond
-          [(or
-             (and (<= char-to-encode 77) (>= char-to-encode 65))
-             (and (<= char-to-encode 109) (>= char-to-encode 97)))
-           (display (integer->char (+ char-to-encode 13)))]
-          [(or
-             (and (<= char-to-encode 90) (>= char-to-encode 78))
-             (and (<= char-to-encode 122) (>= char-to-encode 110)))
-           (display (integer->char (- char-to-encode 13)))]
-          [else
-           (display (integer->char char-to-encode))])))
+  (define (rotate byte)
+    (begin
+      (define char-to-encode (char->integer byte))
+      (cond
+        [(or
+          (and (<= char-to-encode 77) (>= char-to-encode 65))
+          (and (<= char-to-encode 109) (>= char-to-encode 97)))
+         (display (integer->char (+ char-to-encode 13)))]
+        [(or
+          (and (<= char-to-encode 90) (>= char-to-encode 78))
+          (and (<= char-to-encode 122) (>= char-to-encode 110)))
+         (display (integer->char (- char-to-encode 13)))]
+        [else
+         (display (integer->char char-to-encode))])))
 
-    (displayln "Encoded string:")
-    (define (encode-rot13 to-encode)
-      (begin
-        (define to-encode-list (string->list to-encode))
-        (for-each
-          (λ (byte) (rotate byte))
-          to-encode-list)
-        (newline)))
-    (encode-rot13 to-encode)))
-(main)
+  (displayln "Encoded string:")
+  (define (encode-rot13 to-encode)
+    (begin
+      (define to-encode-list (string->list to-encode))
+      (for-each
+       (λ (byte) (rotate byte))
+       to-encode-list)
+      (newline)))
+  (encode-rot13 to-encode))
