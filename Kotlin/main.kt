cc29c5dcaa798dfdea386ad09c31253ba66879b2
wtlgo <50925676+wtlgo@users.noreply.github.com>
2022-08-01 13:59:29 +0300

    A shorter Kotlin version

--- Kotlin/main.kt
+++ Kotlin/main.kt
@@ -1,18 +1,14 @@
 fun main() {
-  println("Enter string to encode:")
-  val line = readLine()
-  var encoded = buildString { }
-  if (line != null) {
-    for (c in line.toCharArray()) {
-      var chr: Char = c
-      if (c in 'A'..'Z') {
-        chr = 'A' + (c - 'A' + 13) % 26
-      } else if (c in 'a'..'z') {
-        chr = 'a' + (c - 'a' + 13) % 26
-      }
-      encoded += chr
-    }
-  }
-  println("Encoded string:")
-  println(encoded)
+    println("Enter string to encode:")
+    val line = readLine() ?: ""
+    val encoded =
+        line.map {
+            when (it) {
+                in 'A'..'A' -> 'A' + (it - 'A' + 13) % 26
+                in 'a'..'z' -> 'a' + (it - 'a' + 13) % 26
+                else -> it
+            }
+        }.joinToString("")
+    println("Encoded string:")
+    println(encoded)
 }
